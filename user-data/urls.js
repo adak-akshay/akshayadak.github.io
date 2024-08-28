785445e85ccb2497c8b7ae22b4647f0559c043f5
akshaykumar.adak <akshaykumar.adak@phonepe.com>
2024-08-29 04:05:47 +0530

    deleted all files

--- user-data/urls.js
+++
@@ -1,6 +0,0 @@
-const githubUsername = "adakakshay";
-
-const createGitConnectedURL = (username) => `https://gitconnected.com/v1/portfolio/${username}`;
-export const URLs = {
-    gitConnected: createGitConnectedURL(githubUsername)
-};
